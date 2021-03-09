76a0499808d1a018de63fa167e5e288a4978a1f4
chengchia <sinchan.yang1221@gmail.com>
2021-03-09 21:54:40 +0800

    add new

---
+++ 123.py
@@ -0,0 +1,11 @@
+#資料型別
+#1.整數 integer int 5 3 1000 
+#2.浮點數(小數) float 0.1 3.145
+#3.布林值 boolean bool (true, false)
+#4.字串 string str 'chengchia' '麵包'
+
+price = 40
+pi = 3.14
+rain = True
+name = 'chengchia'
+print (price, pi, rain, name)
